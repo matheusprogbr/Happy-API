099b0175a7f5588f4745a49b8ee6a226ead39c09
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-14 18:18:06 -0300

    feat: Add files upload feature

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import multer from "multer";
+import uploadConfig from "./config/upload";
+
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
 import OrphanagesController from "./controllers/OrphanagesController";
 
 routes.get("/orphanages", OrphanagesController.index);
 routes.get("/orphanages/:id", OrphanagesController.show);
-routes.post("/orphanages", OrphanagesController.create);
+routes.post("/orphanages", upload.array("images"), OrphanagesController.create);
 
 export default routes;
