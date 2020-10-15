80878efd344e76b9a4dcf6374bd3676948272929
Matheus Henrique <matheusprogbr@gmail.com>
2020-10-15 11:36:34 -0300

    feat: Add Error Handler and Data Validation features

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
+import "express-async-errors";
 import "./database/connection";
 import routes from "./routes";
+import path from "path";
+import cors from "cors";
+import errorHandler from "./errors/handler";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
+server.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+server.use(errorHandler);
 
 server.listen(3333, () => {
   console.log("Server is running!");
